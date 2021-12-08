8f369a1227a44cda190b91d361868bab2d9c2780
DeiberAlexis <daglrsoa@gmail.com>
2021-12-07 20:22:38 -0500

    some changes

--- proyecto/src/components/Login.js
+++ proyecto/src/components/Login.js
@@ -1,4 +1,12 @@
-var login = document.getElementById('login')
-function login(){
+var username = document.querySelector('#username') // dos formas de seleccionar un elemento de html
+
+var password = document.querySelector('#password')
+var licensePlate = document.querySelector('#licensePlate')
+
+// botón de Login
+var loginButton = document.querySelector('#login') // estoy buscando el botton de login
+
+loginButton.addEventListener('click', () => {
     
-}
+}) // recive un evento y una función
+
