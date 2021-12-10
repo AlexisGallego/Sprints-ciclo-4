6270f31c5f9118f9c07e17eb4b166ef3812cf0b9
DeiberAlexis <daglrsoa@gmail.com>
2021-12-09 19:07:17 -0500

    desarrollo de login

--- proyecto/src/components/Login.js
+++ proyecto/src/components/Login.js
@@ -5,8 +5,17 @@ var licensePlate = document.querySelector('#licensePlate')
 
 // botón de Login
 var loginButton = document.querySelector('#login') // estoy buscando el botton de login
+var checkinButton = document.querySelector('#registrarse')
+var recoverPassword = document.querySelector('#Recupararcontraseña')
 
 loginButton.addEventListener('click', () => {
     
 }) // recive un evento y una función
 
+checkinButton.addEventListener('click', () => {
+    return URL
+})
+
+recoverPassword.addEventListener('click', () => {
+    return URL
+})
