d2537541abe89c0b136d601e435d9505e3ec3ffd
DeiberAlexis <daglrsoa@gmail.com>
2021-12-12 18:31:10 -0500

    some changes

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,11 +1,107 @@
-import react from 'react'
-import React from 'react'
-import login from './components/Login'
+import './App.css';
 
 function App() {
-  <div>
-    <h1>learn react</h1>
-  </div>
+  return (
+    <div>
+      <body id="page-top">
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top">Grupo 10</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#!">Registrarse</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Iniciar sesion</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header class="masthead text-center text-white">
+            <div class="masthead-content">
+                <div class="container px-5">
+                    <h1 class="masthead-heading mb-0">COPEAJES</h1>
+                    <h2 class="masthead-subheading mb-0">Te lleva a tu destino</h2>
+                    <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Mostrar Mas</a>
+                </div>
+            </div>
+            <div class="bg-circle-1 bg-circle"></div>
+            <div class="bg-circle-2 bg-circle"></div>
+            <div class="bg-circle-3 bg-circle"></div>
+            <div class="bg-circle-4 bg-circle"></div>
+        </header>
+        <section id="scroll">
+            <div class="container px-5">
+                <div class="text-center">
+                <div class="p-5">
+                  </div><h2 class="display-5">Categoria de cobro</h2>
+                    <div class="text-center">
+                        <div class="p-5"><img class="img-rounded" src="assets/img/05.jpg" alt="...." width="700" height="400" /></div>
+                    </div>                               
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/02.jpg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2 class="display-4">We salute you!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/04.jpeg" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">Let there be rock!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+        <div class="container">
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                    <div class="col-lg-8 col-xl-7">
+                        <form id="contactForm" data-sb-form-api-token="API_TOKEN"></form>
+            <div class="form-floating mb-3">
+                                <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                                <label for="name">Full name</label>
+                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                            </div>
+            </div>
+            </div>
+            
+        </section>
+        
+        
+        <footer class="py-5 bg-black">
+            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; grupo 10</p></div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+    </div>
+  );
 }
 
-export default App
+
+export default App;
--- proyecto/src/components/Login.js
+++ proyecto/src/components/Login.js
@@ -1,4 +1,4 @@
-import App from "../App"
+
 
 var username = document.querySelector('#username') // dos formas de seleccionar un elemento de html
 
@@ -6,18 +6,18 @@ var password = document.querySelector('#password')
 var licensePlate = document.querySelector('#licensePlate')
 
 // botón de Login
-var loginButton = document.getElementById('login') // estoy buscando el botton de login
-var checkinButton = document.querySelector('#registrarse')
-var recoverPassword = document.querySelector('#Recupararcontraseña')
+var loginButton = reactDom.render(document.getElementById('login')) // estoy buscando el botton de login
+var checkinButton = reactDom.render(document.getElementById('checkin'))
+var recoverPassword = ReactDOM.render(document.getElementById('recoverpassword'))
 
-/* loginButton.addEventListener('click', () => {
+ loginButton.addEventListener('click', () => {
 
 }) // recive un evento y una función
 
 checkinButton.addEventListener('click', () => {
-    return URL
+    return 
 })
 
 recoverPassword.addEventListener('click', () => {
-    return URL
-}) */
+    return 
+}) 
